882fd242fab3d0a647e58c21fd415bb86b13e3f4
Shou Ya <shouya@users.noreply.github.com>
2020-05-07 18:39:05 +0800

    various updates

--- scripts/fetch-ical.sh
+++ scripts/fetch-ical.sh
@@ -3,14 +3,35 @@
 set -e
 
 cd "$(dirname "$0")"
-. .credentials
+
+# secrets.sh exports:
+#
+#  - PRIVATE_GOOGLE_CAL_URL
+#  - TARGET_DIARY_FILE
+source secrets.sh
 
 export AUTHOR=Shou
 export EMAIL=shou@lain.li
 export CALENDAR=gcal
 export FILETAGS=WORK
 
-curl "$PRIVATE_GOOGLE_CAL_URL" 2>/dev/null | \
-  awk -f ./ical2org.awk > "$TARGET_CAL_ORG"
+import_ical() {
+  local src dest
+  src="$1"
+  dest="$2"
+
+  import_expr="(icalendar-import-file \"$src\" \"$dest\")"
+  emacs --batch -q --eval "$import_expr"
+}
+
+download_and_import() {
+  curl -o "/tmp/work.ical" "$PRIVATE_GOOGLE_CAL_URL"
+  tmp=$(mktemp)
+  import_ical "/tmp/work.ical" "$tmp"
+  mv "$tmp" "$TARGET_DIARY_FILE"
+}
+
+
+download_and_import
 
 # Run this script periodically
