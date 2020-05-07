8cef5150a7f9b50dd2f82e2bb1702b090392cd81
Shou Ya <shouya@users.noreply.github.com>
2020-05-07 21:30:34 +0800

    trying to live without evil

--- init.el
+++ init.el
@@ -1,1 +1,20 @@
+;; startup time hacks
+
+;; ----------- disable gc on startup -----------
+(setq gc-cons-threshold most-positive-fixnum) ; 2^61 bytes
+;; reset it after load
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold 800000)))
+
+;; ----------- empty file handler alist --------
+(defvar cfg--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq file-name-handler-alist cfg--file-name-handler-alist)))
+(add-hook 'emacs-startup-hook
+          (lambda () (message "Emacs ready in %.2f seconds with %d garbage collections."
+                         (float-time (time-subtract after-init-time before-init-time))
+                         gcs-done)))
+
+
 (org-babel-load-file "~/.emacs.d/preferences.org")
